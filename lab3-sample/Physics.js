deb05899c0ab85b2aa11dd22d1463e08d3dc5e6c
Alistair Alva <a_alva@fanshaweonline.ca>
2024-03-20 03:50:07 -0400

    Changed the circles to an array
    TODO: Fix infinite loop, the circles are not rendering

--- lab3-sample/Physics.js
+++ lab3-sample/Physics.js
@@ -9,22 +9,22 @@ const Physics = (entities, { touches, dispatch, events, time }) => {
   engine.world.gravity.y = 0.1;
 
   // Check if entities.Bridge exists and is properly initialized
-  if (!entities.Bridge || !entities.Bridge.circles) {
+  if (!entities.Bridge || !entities.Bridge.Circles) {
     console.log('No bridge or circles are defined');
     return entities; // Return entities without further processing
   }
 
   // Filter out the circles from the entities
-  let circles = entities.Bridge.circles;
+  let circles = entities.Bridge.Circles;
 
-    console.log('Num of Circles: ', entities.Bridge.circles.length);
+    console.log('Num of Circles: ', circles.length);
   if (circles.length > 1) {
     //Sort the circles by their x position to ensure they're in the correct order
-    circles.sort((a, b) => a.body.position.x - b.body.position.x);
+    //circles.sort((a, b) => a.body.position.x - b.body.position.x);
   
-    //Make the first and last circles static
-    Matter.Body.setStatic(circles[0].body, true);
-    Matter.Body.setStatic(circles[1].body, true);
+    // //Make the first and last circles static
+    // Matter.Body.setStatic(circles[0].body, true);
+    // Matter.Body.setStatic(circles[circles.length -1].body, true);
   
     //Create constraints between each pair of adjacent circles
     for (let i = 1; i < circles.length -1; i++) {
@@ -40,44 +40,7 @@ const Physics = (entities, { touches, dispatch, events, time }) => {
       Matter.World.add(world, constraint1);
     }
   }
-  //////////////// Define connect1 as constraint between Two circles /////////////////////////////////////////////////////////////////
-  // var connect1 = {
-
-  //   bodyA: entities.Circle1.body,
-  //   bodyB: entities.Circle2.body,
-  //   length: 150,        //Distance between two bodies in pixel -- try with some Negative length.
-
-  //   stiffness: 0.2,     // The rate at which it retruns to its resting. 
-  //   //Default is 1 . A value of 1 is very stiff. 0.2 acts like a soft spring. Try 0.5,0.9,2
-
-  //   damping: 0.1,   //the amount of resistance applied to each body based on their velocities to limit the amount of oscillation. 
-  //   //A value of 0.1 means the constraint will apply heavy damping, resulting in little to no oscillation. 
-  //   //A value of 0 means the constraint will apply no damping.
-
-
-  // };
-
-  // var constraint1 = Matter.Constraint.create(connect1); //Passing "connect1" constraint as an argument 
-  // Matter.World.add(world, constraint1);
-
-  //////////////////////////////// constraint2 between a Point A and Circle1 (bodyB)  /////////////////////////////////////////////////////////
-
-  // var constraint2 = Matter.Constraint.create({
-  //   pointA: { x: 180, y: 200 },
-  //   bodyB: entities.Circle1.body,
-  //   //pointB: { x: 0, y: 0 },
-  //   length: 10
-  // });
-
-  //Matter.World.add(world, constraint2);
-
-  //////////////////////////////////////////////////////////////////////////////
-
-
-
-  //////////////////////////////////////////////////////////////////////////////
-
-
+  
   touches
     .filter((t) => t.type === 'press')
     .forEach((t) => {
@@ -87,19 +50,6 @@ const Physics = (entities, { touches, dispatch, events, time }) => {
       });
 
     });
-
-  //////////////////////////////////////////////////////////////////////////////
-
-  // touches
-  //     .filter((t) => t.type === 'press')
-  //     .forEach((t) => {
-  //       Matter.Body.setVelocity(entities.Circle1.body, {
-  //         x: 0, //move along x-axis with given velocity
-  //         y: 10, //move along y-axis with given velocity
-  //       });
-
-  //   });
-  //   Matter.Engine.update(engine, time.delta);
 
   return entities;
 };
--- lab3-sample/entities/index.js
+++ lab3-sample/entities/index.js
@@ -1,21 +1,18 @@
 import { Dimensions } from 'react-native';
-import Circle1 from '../components/Circle1';
+
 import Circle2 from '../components/Circle2';
 import Box from '../components/Box';
 import BoundaryBottom from '../components/BoundaryBottom';
 
 import Matter from 'matter-js';
-//import Constants from "../Constants";
 
 export default (gameWorld) => {
   let engine = Matter.Engine.create();
   let world = engine.world;
-  //engine.gravity.y = 0.0;
-  //engine.gravity.x = 0;
 
   let { width, height } = Dimensions.get('window');
-
   console.log('Width: ', width);
+  
   let circleRadius = 10;
   let circleDiameter = circleRadius * 2;
   let gap = 3;
@@ -26,39 +23,44 @@ export default (gameWorld) => {
 
   console.log('startingX', startingX);
   console.log('totalWidth of circles', totalWidth);
-  let circles = {};
+
+  let circles = [];
 
   // Create the first and last circles
-  circles['Circle0'] = Circle2(world, 'red', { x: startingX, y: height - totalWidth }, circleRadius, {
+  circles.push(Circle2(world, 'red', { x: startingX, y: height - totalWidth }, circleRadius, {
     label: 'Circle2',
     isStatic: true,
-  });
+  }));
   console.log(`Width of circle [0]: ${startingX}\n\n`);
-
-  circles['Circle13'] = Circle2(world, 'red', { x: startingX + (totalCircles -1) * (circleDiameter + gap), y: height - totalWidth }, circleRadius, {
-    label: 'Circle2',
-    isStatic: true,
-  });
-  console.log(`Width of circle [13]: ${startingX + (totalCircles -1) * (circleDiameter + gap)}\n\n`);
-  console.log("Height of static circles: ", height);
 
   // Create the other circles
   for (let i = 1; i < totalCircles - 1; i++) {
-    circles[`Circle${i}`] = Circle2(world, 'green', {
+    circles.push(Circle2(world, 'green', {
       x: startingX + i * (circleDiameter + gap),
       y: height - totalWidth
     },
       circleRadius, {
       label: 'Circle2',
       isStatic: false,
-    });
+    }));
     console.log(`Height of circle ${i} in the loop: ${height}`);
-    console.log(`Horizontal position of circle ${i}: ${startingX + i * (circleDiameter + gap)}\n\n`);
+  console.log(`Horizontal position of circle ${i}: ${startingX + i * (circleDiameter + gap)}\n\n`);
   }
+
+  circles.push(Circle2(world, 'red', { x: startingX + (totalCircles -1) * (circleDiameter + gap), y: height - totalWidth }, circleRadius, {
+    label: 'Circle2',
+    isStatic: true,
+  }));
+  console.log(`Width of circle [${totalCircles -1}]: ${startingX + (totalCircles -1) * (circleDiameter + gap)}\n\n`);
+  console.log("Height of static circles: ", height);
+  console.log(`Size of circle array: ${circles.length}`);
 
   return {
     physics: { engine, world },
 
+    Bridge: {
+      Circles: circles
+    },
     Square: Box(
       world,
       "green",
@@ -66,13 +68,12 @@ export default (gameWorld) => {
       { width: 40, height: 40 },
       { isStatic: false, label: "Enemy" }
     ),
+
     BottomBoundary: BoundaryBottom(
       world,
       'yellow',
       { x: 20, y: height },
       { height: 50, width: 900 }
     ),
-
-    ...circles,
   };
 };
