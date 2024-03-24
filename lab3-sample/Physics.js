822d0228bf7f3e3890f52dbc56ca0a1a260f33ae
Alistair Alva <a_alva@fanshaweonline.ca>
2024-03-24 04:44:07 -0400

    FIXED! Circles are working now
    Moving them to the physics.js file resulted in them having an error. The circles object was not able to be initialized

--- lab3-sample/Physics.js
+++ lab3-sample/Physics.js
@@ -7,37 +7,10 @@ const Physics = (entities, { touches, dispatch, events, time }) => {
   let engine = entities.physics.engine;
   let world = engine.world;
 
-  engine.world.gravity.y = 0.5;
+  engine.world.gravity.y = 0.0;
 
   // Filter out the circles from the entities
-  let circles = entities.circles;
-
-  //if i uncomment this line the app works but the circles fall down once created
-  // if (!circles) {
-  //   console.error('Circles is undefined or null');
-  //   return entities;
-  // }
-
-
-  for (let key in circles) {
-    let previousKey = Number(key) - 1;
-    console.log('Previous Key: ', previousKey);
-    if (previousKey >= 0) {
-      let connect1 = {
-        bodyA: circles[previousKey].body,
-        bodyB: circles[key].body,
-        length: 20,
-        stiffness: 1.0,
-        damping: 0.1,
-      };
   
-      let constraint1 = Matter.Constraint.create(connect1);
-      Matter.World.add(world, constraint1);
-    }
-  
-    console.log('Key: ', key);
-  }
-
   touches
     .filter((t) => t.type === 'press')
     .forEach((t) => {
@@ -53,7 +26,7 @@ const Physics = (entities, { touches, dispatch, events, time }) => {
     .forEach((t) => {
       Matter.Body.setVelocity(entities.Square.body, {
         x: 0, //move along x-axis with given velocity
-        y: 10, //move along y-axis with given velocity
+        y: 15, //move along y-axis with given velocity
       });
 
     });
--- lab3-sample/entities/index.js
+++ lab3-sample/entities/index.js
@@ -10,17 +10,15 @@ import Matter from 'matter-js';
 export default (gameWorld) => {
   let engine = Matter.Engine.create();
   let world = engine.world;
-  engine.gravity.y = 0.5;
-  //engine.gravity.x = 0;
 
   let { width, height } = Dimensions.get('window');
 
   console.log('Width: ', width);
-  let circleRadius = 10;
+  let circleRadius = 15;
   let circleDiameter = circleRadius * 2;
   let gap = 3;
 
-  let totalCircles = 14;
+  let totalCircles = 10;
   let totalWidth = totalCircles * circleDiameter + ((totalCircles -1) * gap);
   let startingX = (width - totalWidth) / 2;
 
@@ -33,7 +31,7 @@ export default (gameWorld) => {
     const isCircleStatic = i === 0 || i === totalCircles -1;
     circles[`Circle${i}`] = Circle2(world, 'green', {
       x: startingX + i * (circleDiameter + gap),
-      y: height - totalWidth
+      y: height - (totalWidth / 2),
     },
       circleRadius, {
       label: 'Circle2',
@@ -41,6 +39,24 @@ export default (gameWorld) => {
     });
     console.log(`Height of circle ${i} in the loop: ${height}`);
     console.log(`Horizontal position of circle ${i}: ${startingX + i * (circleDiameter + gap)}\n\n`);
+  }
+
+  for (let key in circles) {
+    let previousKey = Number(key.slice(6)) - 1;
+    if (previousKey >= 0) {
+      let connect1 = {
+        bodyA: circles[`Circle${previousKey}`].body,
+        bodyB: circles[key].body,
+        length: 30,
+        stiffness: 0.5,
+        damping: 0.1,
+      };
+      console.log('previousKey: ', previousKey);
+      var constraint1 = Matter.Constraint.create(connect1);
+      Matter.World.add(world, constraint1);
+    }
+  
+    console.log('Key: ', key);
   }
 
   return {
