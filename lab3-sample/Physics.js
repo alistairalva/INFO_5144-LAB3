506d41809144d465beb351dab195a147136bb4d7
Alistair Alva <a_alva@fanshaweonline.ca>
2024-03-24 04:05:24 -0400

    Changed ratio of circles

--- lab3-sample/Physics.js
+++ lab3-sample/Physics.js
@@ -7,41 +7,12 @@ const Physics = (entities, { touches, dispatch, events, time }) => {
   let engine = entities.physics.engine;
   let world = engine.world;
 
-  engine.world.gravity.y = 0.5;
-
-  // Filter out the circles from the entities
-  let circles = entities.circles;
-
-  //if i uncomment this line the app works but the circles fall down once created
-  // if (!circles) {
-  //   console.error('Circles is undefined or null');
-  //   return entities;
-  // }
-
-
-  for (let key in circles) {
-    let previousKey = Number(key) - 1;
-    console.log('Previous Key: ', previousKey);
-    if (previousKey >= 0) {
-      let connect1 = {
-        bodyA: circles[previousKey].body,
-        bodyB: circles[key].body,
-        length: 20,
-        stiffness: 1.0,
-        damping: 0.1,
-      };
-  
-      let constraint1 = Matter.Constraint.create(connect1);
-      Matter.World.add(world, constraint1);
-    }
-  
-    console.log('Key: ', key);
-  }
+  engine.world.gravity.y = 0.0;
 
   touches
     .filter((t) => t.type === 'press')
     .forEach((t) => {
-      Matter.Body.setPosition(entities.Square.body, {
+      Matter.Body.setPosition(entities.RectangleRB.body, {
         x: 180, 
         y: 50,
       });
@@ -51,7 +22,7 @@ const Physics = (entities, { touches, dispatch, events, time }) => {
   touches
     .filter((t) => t.type === 'press')
     .forEach((t) => {
-      Matter.Body.setVelocity(entities.Square.body, {
+      Matter.Body.setVelocity(entities.RectangleRB.body, {
         x: 0, //move along x-axis with given velocity
         y: 10, //move along y-axis with given velocity
       });
--- lab3-sample/entities/index.js
+++ lab3-sample/entities/index.js
@@ -1,65 +1,59 @@
-import { Dimensions } from 'react-native';
-import Circle1 from '../components/Circle1';
-import Circle2 from '../components/Circle2';
-import Box from '../components/Box';
-import BoundaryBottom from '../components/BoundaryBottom';
+import Matter from "matter-js";
+import CircleRB from "../components/Circle2";
+import RectangleRB from "../components/Box";
+import { Dimensions } from "react-native";
 
-import Matter from 'matter-js';
-//import Constants from "../Constants";
-
-export default (gameWorld) => {
-  let engine = Matter.Engine.create();
+export default () => {
+  let engine = Matter.Engine.create({ enableSleeping: false });
   let world = engine.world;
-  engine.gravity.y = 0.5;
-  //engine.gravity.x = 0;
 
-  let { width, height } = Dimensions.get('window');
+  engine.world.gravity.y = 0.5;
 
-  console.log('Width: ', width);
-  let circleRadius = 10;
-  let circleDiameter = circleRadius * 2;
-  let gap = 3;
+  const rectangleRB = RectangleRB(
+    world,
+    "blue",
+    { x: Dimensions.get("window").width / 2, y: 50 },
+    { width: 50, height: 50 }
+  );
 
-  let totalCircles = 14;
-  let totalWidth = totalCircles * circleDiameter + ((totalCircles -1) * gap);
-  let startingX = (width - totalWidth) / 2;
+  const circleRBs = [];
+  const numCircles = 16;
+  const circleSpacing = Dimensions.get("window").width / (numCircles + 1);
+  for (let i = 0; i < numCircles; i++) {
+    const isStatic = i === 0 || i === numCircles - 1;
+    const circleX = (i + 1) * circleSpacing;
+    const circleY = Dimensions.get("window").height - 300;
 
-  console.log('startingX', startingX);
-  console.log('totalWidth of circles', totalWidth);
-  const circles = {};
-
-  // Create the other circles
-  for (let i = 0; i < totalCircles; i++) {
-    const isCircleStatic = i === 0 || i === totalCircles -1;
-    circles[`Circle${i}`] = Circle2(world, 'green', {
-      x: startingX + i * (circleDiameter + gap),
-      y: height - totalWidth
-    },
-      circleRadius, {
-      label: 'Circle2',
-      isStatic: isCircleStatic,
-    });
-    console.log(`Height of circle ${i} in the loop: ${height}`);
-    console.log(`Horizontal position of circle ${i}: ${startingX + i * (circleDiameter + gap)}\n\n`);
+    let circleRB = CircleRB(
+      world,
+      "green",
+      { x: circleX, y: circleY },
+      10,
+      isStatic
+    );
+    circleRBs.push(circleRB);
   }
+
+  for (let i = 0; i < numCircles - 1; i++) {
+    Matter.World.add(
+      world,
+      Matter.Constraint.create({
+        bodyA: circleRBs[i].body,
+        bodyB: circleRBs[i + 1].body,
+        length: circleSpacing,
+        stiffness: 0.4,
+      })
+    );
+  }
+
+  const circleEntities = circleRBs.reduce((acc, circle, index) => {
+    acc[`CircleRB_${index}`] = circle;
+    return acc;
+  }, {});
 
   return {
     physics: { engine, world },
-
-    Square: Box(
-      world,
-      "green",
-      { x: 120, y: 120 },
-      { width: 40, height: 40 },
-      { isStatic: false, label: "Enemy" }
-    ),
-    BottomBoundary: BoundaryBottom(
-      world,
-      'yellow',
-      { x: 20, y: height },
-      { height: 50, width: 900 }
-    ),
-
-    ...circles,
+    RectangleRB: rectangleRB,
+    ...circleEntities,
   };
 };
